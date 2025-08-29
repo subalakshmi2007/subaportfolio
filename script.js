25d2d5a7452efd1082ae41758aad3f913102abfe
subalakshmi2007 <subalakshmiragu@gmail.com>
2025-08-29 12:31:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for reaching out, Subalakshmi will get back to you soon!");
+});
